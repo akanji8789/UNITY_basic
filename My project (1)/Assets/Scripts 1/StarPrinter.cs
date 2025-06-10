b33e104c9bac74fee72c8dc7d968eda6a62dc893
akanji8789 <jkh74306208@gmail.com>
2025-06-10 17:20:53 +0900

    STAR
    123123

---
+++ My project (1)/Assets/Scripts 1/StarPrinter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class StarPrinter : MonoBehaviour
+{
+    public int lineCount = 5;
+
+    void Start()
+    {
+        Debug.Log("⭐ 정방향 별 찍기 ⭐");
+        PrintIncreasingStars();
+
+        Debug.Log("⭐ 역방향 별 찍기 ⭐");
+        PrintDecreasingStars();
+    }
+
+    void PrintIncreasingStars()
+    {
+        for (int i = 1; i <= lineCount; i++)
+        {
+            string stars = new string('*', i);
+            Debug.Log(stars);
+        }
+    }
+
+    void PrintDecreasingStars()
+    {
+        for (int i = lineCount; i >= 1; i--)
+        {
+            string stars = new string('*', i);
+            Debug.Log(stars);
+        }
+    }
+}
